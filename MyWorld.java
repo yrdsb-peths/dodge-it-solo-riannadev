acdb49a6bd8764ccc689daed9685ad7304092361
riannadev <riannaknogl@gmail.com>
2026-04-15 11:18:24 -0400

    Added Hero

--- MyWorld.java
+++ MyWorld.java
@@ -5,7 +5,7 @@ public class MyWorld extends World
     public MyWorld() 
     {
         super(600, 400, 1);
-        Hero hero = new Hero ();
+        Hero hero = new Hero();
         addObject(hero, 100, 100);
     }
 }
